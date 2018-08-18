82969a82f39cffa0bfec7609a5555dfa61f65e9b
Zachary Roberts <roberts.zak@gmail.com>
2018-08-18 12:23:57 -0400

    Getting the NPM stuff in sync

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # craft-bootstrap
-A base Craft Project
+A basic Craft 3 Instalation. Includes Gulp for some basic task running. 
+
+
+## Gulp Packages Installed
+- Gulp
+- Gulp Sass
+- Browser Sync
+- Gulp Concat
+- Gulp Rename
+- Gulp Uglify
+- Gulp CSS Nano
+- Gulp Image Min
+- Gulp Cache
+- Del
+- Run Sequence
+- Gulp Auto-prefixer
+
+## Gulp Pre-Configured Commands
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,30 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
 
 gulp.task('sass', function(){
-	return gulp.src([
-		'web/source/scss/**/*.scss',
-		//'!web/source/scss/**/_*.scss'
-		])
+	return gulp.src('web/source/scss/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('web/dist/css'))
-})
-gulp.task('watch', function(){
-	gulp.watch('web/source/**/*.scss', ['sass']);	
-})
+		.pipe(browserSync.reload({
+			stream: true
+		}))
+});
+gulp.task('browserSync', function(){
+	browserSync.init({
+		server: {
+			baseDir: 'web'
+		},
+	})
+});
+gulp.task('useref', function(){
+	return gulp.src('templates/*.twig')
+		.pipe(useref())
+		.pipe(gulp.dest('dist'))
+});
+gulp.task('watch', ['browserSync', 'sass'] function(){
+	gulp.watch('web/source/**/*.scss', ['sass']);
+	gulp.watch('templates/**/*.twig', browserSync.reload);
+	gulp.watch('web/**/*.js', browserSync.reload);	
+});
