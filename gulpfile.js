651f72fe0bdcd44c45a5b632fbcec98e2ccee5e6
Zachary Roberts <roberts.zak@gmail.com>
2018-08-17 22:07:18 -0400

    typo correction

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function(){
-	return hulp.src([
+	return gulp.src([
 		'web/source/scss/**/*.scss',
 		//'!web/source/scss/**/_*.scss'
 		])
